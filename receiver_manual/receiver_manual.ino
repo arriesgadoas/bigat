169ecb86ce72852b4625a0211766171c89e43fdf
ejcmateo <87356842+ejcmateo@users.noreply.github.com>
2021-11-22 14:26:09 +0800

    Update receiver_manual.ino
    Added packet size checker

--- receiver_manual/receiver_manual.ino
+++ receiver_manual/receiver_manual.ino
@@ -8,24 +8,25 @@
 #define LORA_MOSI 27    // GPIO27 -  SX1276 MOSI
 #define LORA_CS 18     // GPIO18 -   SX1276 CS
 #define LORA_RST 14   // GPIO14 -    SX1276 RST
-#define LORA_IRQ 26  // GPIO26 -     SX1276 IRQ (interrupt request)
+#define LORA_IRQ 26  // GPIO26 -     SX1276 IRQ (interrupt request)U
 
 typedef struct packet {
   byte key;                       //passkey
   byte command;                   //0,1,2, or 3
   byte level;                     //level assigend to a node
   byte id;                        //node id must be unique
-  byte path[10];                  //track packet path[sorce_node id, 2nd_hop_node id, 3rd_hop_node id,...]
+  int path[10];                  //track packet path[sorce_node id, 2nd_hop_node id, 3rd_hop_node id,...]
 };
 
 
 struct packet packet_t;
+int packet_t_size = sizeof(packet_t);
 
 struct peakAccel {
   byte key;
   byte id;
   byte level;
-  byte path[10];
+  int path[10];
   double lat;                 // Latitude
   double lng;                 // Longitude
   int32_t alt;                // Altitude in cm
@@ -42,6 +43,7 @@ struct peakAccel {
 };
 
 struct peakAccel pgaData;
+int pgaData_size = sizeof(pgaData);
 
 byte id;
 byte level;
@@ -77,7 +79,7 @@ void setupLora() {
 
   }
 
-  LoRa.setSpreadingFactor(12);
+  LoRa.setSpreadingFactor(7);
   LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);
 }
 
@@ -110,35 +112,48 @@ void getNodes() {
   int packetSize = LoRa.parsePacket ();
   if (packetSize) // Only read if there is some data to read..
   {
-    LoRa.readBytes((uint8_t *)&packet_t, packetSize);
-    if (packet_t.key = 83) {
-      id = packet_t.id;
-      toSerial = "Node connected: " + String(packet_t.id);
-      Serial.println(toSerial);
+    if (packetSize == packet_t_size) {
+      LoRa.readBytes((uint8_t *)&packet_t, packetSize);
+      if (packet_t.key = 83) {
+        id = packet_t.id;
+        toSerial = "Node connected: " + String(packet_t.id);
+        Serial.println(toSerial);
+      }
+    }
+    else {
+      LoRa.flush();
     }
   }
 }
 
 void setupNetwork() {
-  int packetSize = LoRa.parsePacket ();
+  int packetSize = LoRa.parsePacket();
   if (packetSize) // Only read if there is some data to read..
   {
-    LoRa.readBytes((uint8_t *)&packet_t, packetSize);
-
-    if (packet_t.key = 83) {
-      path = "";
-      if (packet_t.path[0] != 0) {
-        id = packet_t.id;
-        level = packet_t.level;
-        for (int i = 0; i < 10; i++) {
-          path += String(packet_t.path[i]);
-          if (i != 9) {
-            path += "-";
+    Serial.print("RECEIVED ");
+    // Crashing here
+    if (packetSize == packet_t_size) {
+      LoRa.readBytes((uint8_t *)&packet_t, packetSize);
+    
+      if (packet_t.key == 83) {
+        path = "";
+        Serial.print("VALID :");
+        if (packet_t.path[0] != 0) {
+          id = packet_t.id;
+          level = packet_t.level;
+          for (int i = 0; i < 10; i++) {
+            path += String(packet_t.path[i]);
+            if (i != 9) {
+              path += "-";
+            }
           }
+          toSerial = "{id:" + String(id) + "," + "level:" + String(level) + "," + "path:" + path + "}";
+          Serial.println(toSerial);
         }
-        toSerial = "{id:" + String(id) + "," + "level:" + String(level) + "," + "path:" + path + "}";
-        Serial.println(toSerial);
       }
+    }
+    else {
+      LoRa.flush();
     }
   }
 }
@@ -148,32 +163,37 @@ void stopLogging() {
   int packetSize = LoRa.parsePacket ();
   if (packetSize) // Only read if there is some data to read..
   {
-    Serial.print("RECEIVED:");
-    LoRa.readBytes((uint8_t *)&pgaData, packetSize);
-    Serial.println(pgaData.key);
-    if (pgaData.key == 84) {
-      path = "";
-      if (pgaData.path[0] != 0) {
-        id = pgaData.id;
-        level = pgaData.level;
-        for (int i = 0; i < 10; i++) {
-          path += String(pgaData.path[i]);
-          if (i != 9) {
-            path += "-";
+    if (packetSize == pgaData_size) {
+      Serial.print("RECEIVED:");
+      LoRa.readBytes((uint8_t *)&pgaData, packetSize);
+      Serial.println(pgaData.key);
+      if (pgaData.key == 84) {
+        path = "";
+        if (pgaData.path[0] != 0) {
+          id = pgaData.id;
+          level = pgaData.level;
+          for (int i = 0; i < 10; i++) {
+            path += String(pgaData.path[i]);
+            if (i != 9) {
+              path += "-";
+            }
           }
         }
+        lat_ = pgaData.lat;
+        lng_ = pgaData.lng;
+        alt = pgaData.alt;
+        date_ = pgaData.date;
+        time_ = pgaData.time;
+        ax = pgaData.ax;
+        ay = pgaData.ay;
+        az = pgaData.az;
+        mag_calc = pgaData.mag_calc;
+        toSerial = "{id:" + String(id) + "," + "level:" + String(level) + "," + "path:" + path + "," + "lat:" + String(lat_) + "," + "lng:" + String(lng_) + "," + "alt:" + String(alt) + "\n" + "date:" + String(date_) + "," + "time:" + String(time_) + "," + "mag:" + String(mag_calc,10) + "}";
+        Serial.println(toSerial);
       }
-      lat_ = pgaData.lat;
-      lng_ = pgaData.lng;
-      alt = pgaData.alt;
-      date_ = pgaData.date;
-      time_ = pgaData.time;
-      ax = pgaData.ax;
-      ay = pgaData.ay;
-      az = pgaData.az;
-      mag = pgaData.mag;
-      toSerial = "{id:" + String(id) + "," + "level:" + String(level) + "," + "path:" + path + "," + "lat:" + String(lat_) + "," + "lng:" + String(lng_) + "," + "alt:" + String(alt) + "\n" + "date:" + String(date_) + "," + "time:" + String(time_) + "," + "mag:" + String(mag_calc,10) + "}";
-      Serial.println(toSerial);
+    }
+    else {
+      LoRa.flush();
     }
   }
 }
